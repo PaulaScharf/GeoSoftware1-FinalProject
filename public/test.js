4e02eee9d1b8b919f278cf0c1b6a4ad2e104f94f
KPoppi <katharina.poppinga@gmail.com>
2019-08-02 20:31:32 +0200

    generated (not finished) test- and Docker files

---
+++ public/test.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+// jshint maxerr: 1000
+
+"use strict";  // JavaScript code is executed in "strict mode"
+
+/**
+* @desc final project, Geosoftware1, SoSe2019
+* @author name: Katharina Poppinga, matr.: 450 146; name: Paula Scharf, matr.: 450 334
+*/
+
+
+// ********** qUnit client-Tests **********
+
+
+// ...
---
+++ test/test.js
@@ -0,0 +1,26 @@
+// jshint esversion: 8
+// jshint node: true
+// jshint maxerr: 1000
+
+"use strict";  // JavaScript code is executed in "strict mode"
+
+
+/**
+* @desc final project, Geosoftware1, SoSe2019
+* @author name: Katharina Poppinga, matr.: 450 146; name: Paula Scharf, matr.: 450 334
+*/
+
+
+
+// ********** Mocha server-Tests **********
+
+
+var assert = require("assert");
+
+var http = require("http");
+
+let portNumber = 3000;
+
+
+
+// ...
